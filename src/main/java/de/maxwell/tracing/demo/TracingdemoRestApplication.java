dccd1e3346e854ef1ae5ae3d8e537e7099bfd473
max.weis <max.weis@openknowledge.de>
2020-06-30 14:20:20 +0200

    init commit

---
+++ src/main/java/de/maxwell/tracing/demo/TracingdemoRestApplication.java
@@ -0,0 +1,11 @@
+package de.maxwell.tracing.demo;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class TracingdemoRestApplication extends Application {
+}
